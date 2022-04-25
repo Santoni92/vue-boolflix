2c63af8e654fa0cd5ac10ffbfc1d5134793a3f03
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-25 18:34:54 +0200

    aggiunto l'hover sulle copertine

--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -1,39 +1,65 @@
 <template>
-  <div :style="backgroundImageInlineStyle">
-        <!--div v-if="film.poster_path !== null">
-            <img :src="'https://image.tmdb.org/t/p/' + 'w342/' + film.poster_path" :alt="film.title">
-        </div>
-        <div v-else>Copertina non disponibile</div-->
-        <h3>{{ film.title }}</h3>
-        <h4>{{ film.original_title }}</h4>
-        <p>{{ film.vote_average }}</p>
-        <FlagComponent :flags="film.original_language"/>
-        <StarsComponent :voto="film.vote_average"/>
+  <div>
+    <div class="card">
+      <div
+        v-if="film.poster_path !== null "
+        :style="backgroundImageInlineStyle"
+        class="background"
+      >
+        <!--img :src="'https://image.tmdb.org/t/p/' + 'w342/' + film.poster_path" :alt="film.title"-->
+      </div>
+      <div v-else>
+        Copertina non disponibile
+      </div>
+    </div>
+    <div class="informazioni">
+       <h3>{{ film.title }}</h3>
+       <h4>{{ film.original_title }}</h4>
+       <p>{{ film.vote_average }}</p>
+      <FlagComponent :flags="film.original_language" />
+      <StarsComponent :voto="film.vote_average" />
+    </div>
   </div>
 </template>
 
 <script>
-import FlagComponent from './FlagComponent.vue'
-import StarsComponent from './StarsComponent.vue'
+import FlagComponent from "./FlagComponent.vue";
+import StarsComponent from "./StarsComponent.vue";
 export default {
-    name:'FilmItem',
-    props:{
-        film:Object
-    },
-    computed:{
-        backgroundImageInlineStyle(){
-            return `background-image: url('https://image.tmdb.org/t/p/' + 'w342/' + ${this.film.poster_path})`;
-        }
-    },
-    components:{
-        FlagComponent,
-        StarsComponent
+  name: "FilmItem",
+  props: {
+    film: Object,
+  },
+  computed: {
+    backgroundImageInlineStyle() {
+      //console.log(`https://image.tmdb.org/t/p/w342/${this.film.poster_path}`);
+      return `background-image: url(https://image.tmdb.org/t/p/w342/${this.film.poster_path})`;
     }
-}
+  },
+  components: {
+    FlagComponent,
+    StarsComponent,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
- img{
-     width:100%;
- }
+
+  .background {
+    min-height: 342px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .informazioni{
+    min-height: 342px;
+    display:none;
+  }
+  .card:hover .background{
+    display:none;
+  }
+
+  .card:hover .informazioni{
+    display:block
+  }
 </style>
--- src/components/SerieItem.vue
+++ src/components/SerieItem.vue
@@ -1,15 +1,19 @@
 <template>
 <div>
-     <div v-if="serieTv.poster_path !== null">
-        <img :src="'https://image.tmdb.org/t/p/' + 'w342/' + serieTv.poster_path" :alt="serieTv.original_name">
-     </div>
-     <div v-else>Copertina non disponibile</div>
-     <h3>{{ serieTv.name }}</h3>
-     <h4>{{ serieTv.original_name }}</h4>
-     <!--p>{{ serieTv.original_language }}</p-->
-     <p>{{ serieTv.vote_average }}</p>
-     <FlagComponent :flags="serieTv.original_language"/>
-     <StarsComponent :voto="serieTv.vote_average"/>
+    <div class="card">
+        <div v-if="serieTv.poster_path !== null"  :style="backgroundImageInlineStyle"
+             class="background">
+        </div>
+        <div v-else>Copertina non disponibile</div>
+    </div>
+    <div class="informazioni">
+        <h3>{{ serieTv.name }}</h3>
+        <h4>{{ serieTv.original_name }}</h4>
+        <!--p>{{ serieTv.original_language }}</p-->
+        <p>{{ serieTv.vote_average }}</p>
+        <FlagComponent :flags="serieTv.original_language"/>
+        <StarsComponent :voto="serieTv.vote_average"/>
+    </div>  
 </div>
 </template>
 
@@ -21,6 +25,13 @@ export default {
     props:{
         serieTv:Object
     },
+     computed: {
+                backgroundImageInlineStyle()
+                 {
+                     //console.log(`https://image.tmdb.org/t/p/w342/${this.film.poster_path}`);
+                     return `background-image: url(https://image.tmdb.org/t/p/w342/${this.serieTv.poster_path})`;
+                 }
+             },
     components:{
         FlagComponent,
         StarsComponent
@@ -29,7 +40,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-img{
-    width:100%;
+.background {
+  min-height: 342px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.informazioni{
+  min-height: 342px;
+  display:none;
+}
+.card:hover .background{
+  display:none;
+}
+.card:hover .informazioni{
+  display:block
 }
 </style>
